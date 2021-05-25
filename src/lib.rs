a8e2c1726bbd157df5e76b128eac290afcc18655
dragsama <dragsamax@gmail.com>
2021-05-25 21:59:04 +0530

    Add base dispatcher, command handler (will add dispatcher, handler traits later)

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![allow(unused_imports)]
 #![allow(dead_code)]
+#![allow(unused_variables)]
 
 extern crate reqwest;
 extern crate serde_json;
@@ -22,10 +23,40 @@ impl Bot {
     }
 }
 
+pub struct CommandHandler {
+    pub command: String,
+    pub func: fn(&Update)
+}
+
+impl CommandHandler {
+    pub fn check_update(&self, update: &Update){
+        true;
+    }
+}
+
+pub struct Dispatcher {
+    pub handlers: Vec<CommandHandler>
+}
+
+impl Dispatcher {
+    pub fn new() -> Self {
+        Self {
+            handlers: Vec::new()
+        }
+    }
+    pub fn add_handler(&mut self, handler: CommandHandler){
+        self.handlers.push(handler);
+    }
+    pub async fn handle_update(&self, update: &Update){
+        println!("{:#?}", update);
+    }
+}
+
 pub struct Updater {
     pub bot: Bot,
     pub base_endpoint: String,
     pub reqwest_client: reqwest::Client,
+    pub dispatcher: Dispatcher
 }
 
 impl Updater {
@@ -34,13 +65,11 @@ impl Updater {
             bot: Bot::new(bot_token.into()),
             base_endpoint: format!("https://api.telegram.org/bot{}/", bot_token).into(),
             reqwest_client: reqwest::Client::new(),
+            dispatcher: Dispatcher::new()
         }
     }
 
-    pub async fn handle_update(&self, update: &Update){
-       println!("Got update: {:#?}", update)
-    }
-    pub async fn start_polling(self){
+    pub async fn start_polling(&self){
         let mut offset: i64 = 0;
         loop {
             let url = format!("{}{}?offset={}", self.base_endpoint, "getUpdates", offset+1);
@@ -50,7 +79,7 @@ impl Updater {
             let resp = serde_json::from_str::<Updates>(&result).unwrap();
             println!("Working 3");
             for update in resp.result.iter() {
-                self.handle_update(update).await;
+                self.dispatcher.handle_update(update).await;
                 offset = max(offset, update.update_id);
             }
             println!("Working 4");
