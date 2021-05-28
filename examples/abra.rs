12c266825c0db3c099bebc9dcca6b14c659fe611
dragsama <dragsamax@gmail.com>
2021-05-28 19:39:25 +0530

    push broken stuff

--- examples/abra.rs
+++ examples/abra.rs
@@ -1,10 +1,12 @@
-extern crate rust_tg_bot;
+use rust_tg_bot::{Updater, CommandHandler, types::update::Update, traits::dispatcher::Dispatcher};
 
 use std::env;
 
+async fn echo(&Update){}
+
 #[tokio::main]
 async fn main(){
-    let bot = rust_tg_bot::Updater::new(&env!("TOKEN").to_string());
-    // bot.send_message(660565862, "a").await;
+    let bot = Updater::new(&env!("TOKEN").to_string());
+    bot.dispatcher.add_handler(CommandHandler{command: "echo", func: echo});
     bot.start_polling().await;
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,59 +5,60 @@
 extern crate reqwest;
 extern crate serde_json;
 
+pub mod types;
 pub mod methods;
 pub mod traits;
-pub mod types;
 
-use crate::{
-    traits::dispatcher::{Dispatcher as dp_trait, Handler},
-    types::{Update, Updates},
-};
+use crate::{types::{Update, Updates}, traits::dispatcher::{Dispatcher as dp_trait, Handler}};
 use async_trait::async_trait;
-use std::cmp::max;
+use std::{cmp::max};
 
 pub struct Bot {
-    pub token: String,
+    pub token: String
 }
 
 impl Bot {
     pub fn new(bot_token: &str) -> Self {
         Self {
-            token: bot_token.into(),
+            token: bot_token.into()
         }
     }
 }
 
 pub struct CommandHandler {
     pub command: String,
-    pub func: fn(&Update),
+    pub func: Fn(&Update)
 }
 
-impl CommandHandler {
-    pub fn check_update(&self, update: &Update) {
-        true;
+#[async_trait]
+impl Handler for CommandHandler {
+    async fn check_update(&self, update: &Update){
+        println!("{:#?}", update.message.as_ref().unwrap().chat)
     }
 }
 
 pub struct Dispatcher {
-    pub handlers: Vec<Box<dyn Handler>>,
+    pub handlers: Vec<Box<dyn Handler>>
 }
 
 impl Dispatcher {
     pub fn new() -> Self {
         Self {
-            handlers: Vec::new(),
+            handlers: Vec::new()
         }
     }
 }
 
 #[async_trait]
 impl dp_trait for Dispatcher {
-    fn add_handler(&mut self, handler: Box<dyn Handler>, group: Option<i32>) {
+    fn add_handler(&mut self, handler: Box<dyn Handler>, group: Option<i32>){
         self.handlers.push(handler);
     }
-    async fn handle_update(&self, update: Update) {
+    async fn handle_update(&self, update: Update){
         println!("{:#?}", update);
+        for handler in self.handlers.iter(){
+	handler.check_update(&update);
+        }
     }
 }
 
@@ -65,7 +66,7 @@ pub struct Updater {
     pub bot: Bot,
     pub base_endpoint: String,
     pub reqwest_client: reqwest::Client,
-    pub dispatcher: Dispatcher,
+    pub dispatcher: Dispatcher
 }
 
 impl Updater {
@@ -74,34 +75,21 @@ impl Updater {
             bot: Bot::new(bot_token.into()),
             base_endpoint: format!("https://api.telegram.org/bot{}/", bot_token).into(),
             reqwest_client: reqwest::Client::new(),
-            dispatcher: Dispatcher::new(),
+            dispatcher: Dispatcher::new()
         }
     }
 
-    pub async fn start_polling(&self) {
+    pub async fn start_polling(&self){
         let mut offset: i64 = 0;
         loop {
-            let url = format!(
-                "{}{}?offset={}",
-                self.base_endpoint,
-                "getUpdates",
-                offset + 1
-            );
+            let url = format!("{}{}?offset={}", self.base_endpoint, "getUpdates", offset+1);
             println!("Working 1");
-            let result = self
-                .reqwest_client
-                .get(&url)
-                .send()
-                .await
-                .unwrap()
-                .text()
-                .await
-                .unwrap();
+            let result = self.reqwest_client.get(&url).send().await.unwrap().text().await.unwrap();
             println!("Working 2");
             let resp = serde_json::from_str::<Updates>(&result).unwrap();
             println!("Working 3");
             for update in resp.result {
-                offset = max(offset, update.update_id);
+	offset = max(offset, update.update_id);
                 self.dispatcher.handle_update(update).await;
             }
             println!("Working 4");
